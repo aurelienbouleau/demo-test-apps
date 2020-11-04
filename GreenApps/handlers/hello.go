9490551eaca7b819c52eeb7cf3c4e11719249bf4
Drouard Ludovic <ludovic.drouard@orange.com>
2020-11-04 10:38:43 +0000

    Add Green Apps

---
+++ GreenApps/handlers/hello.go
@@ -0,0 +1,34 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/cloudfoundry-samples/test-app/helpers"
+)
+
+type Hello struct {
+	Time time.Time
+}
+
+func (p *Hello) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	index, _ := helpers.FetchIndex()
+
+	str := "abcdefghijklmnopqrstuvwxyz-"
+        res := ""
+        for i := 1; i < 650; i++ {
+           res = res + str
+        }
+	styledTemplate.Execute(w, Body{Body: fmt.Sprintf(`
+<div class="hello">
+	Test App	
+</div>
+
+<div class="my-index">My Index Is</div>
+
+<div class="index">%d</div>
+<div class="mid-color">Uptime: %s</div>
+<div class="bottom-color"></div>
+    `, index, time.Since(p.Time))})
+}
